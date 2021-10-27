8cbecc7f616b67c2b21c7a5d80a80b8c72b15de7
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-10-27 14:49:00 -0400

    Format code with black

--- xrftomo_scaninfo.py
+++ xrftomo_scaninfo.py
@@ -1,28 +1,30 @@
+# flake8: noqa
+
 import numpy as np
 import time as ttime
 
-def get_tomo_information(start, stop, fn='tomo_info.dat'):
+
+def get_tomo_information(start, stop, fn="tomo_info.dat"):
     # Make a list of scan IDs
-    ids = list(np.arange(start, stop+1))
+    ids = list(np.arange(start, stop + 1))
 
-    header = 'Start Time,Scan ID,Theta,Use,Filename,X Start,X Stop,Num X,Y Start,Y Stop,Num Y,Dwell,UID,Status\n'
+    header = "Start Time,Scan ID,Theta,Use,Filename,X Start,X Stop,Num X,Y Start,Y Stop,Num Y,Dwell,UID,Status\n"
 
-    with open(fn, 'a') as f:
+    with open(fn, "a") as f:
         f.write(header)
         ind = 0
-        for scanid in ids: 
+        for scanid in ids:
             h = db[int(scanid)]
-            textout = '' 
-            textout = ttime.ctime(h.start['time']) 
-            textout = textout + ',' + str(h.start['scan_id']) 
+            textout = ""
+            textout = ttime.ctime(h.start["time"])
+            textout = textout + "," + str(h.start["scan_id"])
             # textout = textout + ',' + str(h.start['theta'])
-            textout = textout + ',' + str(th[ind])  
-            textout = textout + ',x' 
-            textout = textout + ',' + f'scan2D_{scanid}_xs_sum4ch.h5'
-            textout = textout + ',' + str(h.start['scan_input'])[1:-1].replace(' ', '') 
-            textout = textout + ',' + h.start['uid'] 
-            textout = textout + ',' + h.stop['exit_status'] + '\n'
+            textout = textout + "," + str(th[ind])
+            textout = textout + ",x"
+            textout = textout + "," + f"scan2D_{scanid}_xs_sum4ch.h5"
+            textout = textout + "," + str(h.start["scan_input"])[1:-1].replace(" ", "")
+            textout = textout + "," + h.start["uid"]
+            textout = textout + "," + h.stop["exit_status"] + "\n"
             f.write(textout)
-            if (h.stop['exit_status'] == 'success'):
+            if h.stop["exit_status"] == "success":
                 ind = ind + 1
-
